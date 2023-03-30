e5d73e53cc26f05b4457d0d458a7b4f8cd69a5a1
anmolsinghturka <anmolsinghturka@gmail.com>
2023-03-30 02:13:54 -0700

    added enable cors for live server hosting

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.enableCors();
+
     app.setGlobalPrefix('/api');
 
     const config = new DocumentBuilder()
